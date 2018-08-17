570f0789dd05e8fdda5077631608b00f7cb351ff
Calvin Williams <40271790+sc-CalvinWilliams@users.noreply.github.com>
2018-08-17 12:34:53 -0400

    Init patch

---
+++ src/Sitecore.Support.13915/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.13915")]
+[assembly: AssemblyProduct("Sitecore.Support.13915")]
+[assembly: ComVisible(false)]
